c0345076b42048a240be3731b3834ba128a123c0
luxifu1989 <liuqingchen19892gmail.com>
2021-12-14 15:49:09 +0100

    rewrite codes

--- src/voronoi_draw/scripts/BLF_Controller.py
+++ src/voronoi_draw/scripts/BLF_Controller.py
@@ -3,6 +3,14 @@ import rospy
 import math
 from voronoi_custom import getConvexBndMatrix 
 
+class NeighborInfo:
+	neighborID = 0
+	vm_coord_2d = 0
+	cvt_coord_2d = 0
+	com_v1_2d = 0
+	com_v2_2d = 0
+
+
 # State Feedback from the sensos, qualisys, ...
 class BLF_Controller:
 	def __init__(self):
@@ -48,7 +56,6 @@ class BLF_Controller:
 		self.VmY = self.PosY + 200 * math.cos(self.Theta)
 
 	def updateState(self, data):	
-		#rospy.loginfo("Update new Info Agent: %d", agent.ID)
 		self.ID = np.int32(data.packet.TransmitterID)
 		self.PosX = np.float32(data.packet.AgentPosX)
 		self.PosY = np.float32(data.packet.AgentPosY)
@@ -57,39 +64,6 @@ class BLF_Controller:
 		# Update this internally for centralized controller
 		self.updateVM(200)	# Update the virtual center with radius 200 locally to avoid conflict with another node
 		
-	# def updateBLFState(self, newCentroidX, newCentroidY):
-	# 	self.TargetX = newCentroidX
-	# 	self.TargetY = newCentroidY
-	# 	posFilterThres = 0
-	# 	dx = self.VmX - self.lastVmX
-	# 	dy = self.VmY - self.lastVmY
-	# 	isValid = abs(dx) > posFilterThres and abs(dy) > posFilterThres
-	# 	if(isValid):
-	# 		nBndLines = len(self.bBnd)
-	# 		tmpV = 0
-	# 		# Compute own BLF function
-	# 		for j in range(nBndLines):
-	# 			# Multiply by 10000 to increase accuracy 
-	# 			newS = 10000 // (-1 * self.bBnd[j,0] - self.ABnd[j,0]*self.VmX - self.ABnd[j,1]*self.VmY)
-	# 			tmpV += newS
-	# 			#if(self.ID == 20005):
-	# 			#	rospy.loginfo([self.ABnd[j,0], self.ABnd[j,1], -1 * self.bBnd[j,0], self.VmX, self.VmY, newS, tmpV])
-
-	# 		innerProd = math.pow(self.VmX - self.TargetX, 2) + math.pow(self.VmY - self.TargetY, 2)
-	# 		V = tmpV * innerProd 
-			
-	# 		# This condition guarantees that agent maintains inside the coverage region
-	# 		if(V < 0):
-	# 			rospy.logwarn("ID: %d -> BLF violated", self.ID)
-
-	# 		# Update the distributed state and return to the centralized controller
-	# 		self.dVBLF = V - self.lastVBLF
-	# 		self.lastVBLF = V
-	# 	else:
-	# 		self.dVBLF = 0
-
-	# 	return self.dVBLF
-
 	def updateLyapunov(self, newCVT_2d, V, dV):
 		self.TargetX = newCVT_2d[0]
 		self.TargetY = newCVT_2d[1]
@@ -97,6 +71,29 @@ class BLF_Controller:
 		self.lastVBLF = V
 		return 0
 		
+	# # Obain the new Voronoi information and return the partial derivatives / or Lyapuno feedback for adjacent agents
+	# def updateVoronoiInfo(self, myCVT, adjPart):
+	# 	nNeighbor = len(adjPart)
+
+	# 	dCi_dzi = 0
+	# 	for i in range(nNeighbor):
+	# 		myZ = np.array([self.VmX, self.VmY])
+	# 		myCVT = np.array([self.VmX, self.VmY])
+	# 		dCi_dzi_AdjacentJ, dCi_dzj = Voronoi2D_calCVTPartialDerivative(, myAgent.C, partitionMasses[agentID],\
+	# 									adjCoord_2d, commonverMat[agentID][neighborID][0], commonverMat[agentID][neighborID][1])
+	# 		#myAgent.dCi_dzi += dCi_dzi_AdjacentJ
+	# 		#dCi_dzj_list[agentID,neighborID,:,:] = dCi_dzj[:,:]
+		
+	# 	for neighborID in range(0, self._nAgent):
+	# 		if(self.adjacentMat[agentID][neighborID] == 1):
+	# 			adjCoord_2d = np.array([self._AgentList[neighborID].VmX, self._AgentList[neighborID].VmY])
+	# 			dCi_dzi_AdjacentJ, dCi_dzj = Voronoi2D_calCVTPartialDerivative(myAgent.z, myAgent.C, partitionMasses[agentID],\
+	# 										adjCoord_2d, commonverMat[agentID][neighborID][0], commonverMat[agentID][neighborID][1])
+	# 			myAgent.dCi_dzi += dCi_dzi_AdjacentJ
+	# 			dCi_dzj_list[agentID,neighborID,:,:] = dCi_dzj[:,:]
+
+
+	# 	return 0
 
 	def begin(self, controlGain, v0, w0, wCO, boundaries):
 		self.gain = controlGain
@@ -121,9 +118,6 @@ class BLF_Controller:
 		self.wOrbit = 30
 		eps = 5
 		# Control output ====================================
-		# Controller selection
-		#w = self.simpleController()
-		#		w = self.__controlBLF(neighborInfo)
 		w = self.wOrbit + self.gain * calc_sigmoid(dV[0] * math.cos(self.Theta) + dV[1] * math.sin(self.Theta), eps)
 
 		# Output cutoff
@@ -135,7 +129,7 @@ class BLF_Controller:
 			rospy.logwarn("ANGULAR THRESHOLD VIOLATED: %.2f !", w)
 
 		# Constant Heading Velocity
-		self.vConst = 10
+		self.vConst = 16
 		self.angularVel = w
 		return [self.vConst, self.angularVel]
 
@@ -149,33 +143,12 @@ class BLF_Controller:
 		V = tmpV * (math.pow(self.VmX - self.TargetX, 2) + math.pow(self.VmY - self.TargetY, 2)) / 2 
 		return V
 
-	def simpleController(self):
+	def computeSimple(self):
 		w = self.wOrbit + self.gain * np.sign((self.VmX - self.TargetX) * math.cos(self.Theta) 
 							+ (self.VmY - self.TargetY) * math.sin(self.Theta))
 		return w
 
-	# This method is only using simple Euler derivative, however can be improved by theoretical integration 
-	# and update the algorithm
-	# def __controlBLF(self, neighborInfo):
-	# 	#rospy.logwarn([neighborInfo, np.array(neighborInfo[:,1]), sum(np.array(neighborInfo[:,1]))])
-	# 	# Some filter position here
-	# 	posFilterThres = 0
-	# 	dx = self.VmX - self.lastVmX
-	# 	dy = self.VmY - self.lastVmY
-	# 	if(np.array(neighborInfo).size == 0):
-	# 		rospy.logwarn("No neighbor detected ! Double check Adjacent list")
-	# 		return self.angularVel
-	# 	else:
-	# 		isValid = abs(dx) > posFilterThres and abs(dy) > posFilterThres
-	# 		if(isValid):
-	# 			dV_total = (self.dVBLF + sum(np.array(neighborInfo[:,1]))) # Add this term to avoid division to the small number in the following lines
-	# 			dVdzx = dV_total / np.double(dx)
-	# 			dVdzy = dV_total / np.double(dy)
-	# 			w = self.wOrbit + self.gain * np.sign(dVdzx * math.cos(self.Theta) + dVdzy * math.sin(self.Theta))
-	# 			#rospy.loginfo("%d dV: %.7f dx: %.7f dy: %.7f, sumAdj: %.3f", self.ID, dV_total, dx, dy, dV_Adjacent)
-	# 			return w
-	# 		else:
-	# 			return self.angularVel	# If nothing happened, just keep the old angular velocity
+	
 		
 def calc_sigmoid(x, eps):
 	return x / (abs(x) + eps)
